4486f543feb919ab3b662562f9c9ba157fc46210
wessgitley <wesmut00@gmail.com>
2026-01-18 15:49:00 +0300

    My Introduction

---
+++ PersonalIntroduction.java
@@ -0,0 +1,7 @@
+public class PersonalIntroduction {
+  public static void main(String[] args) {
+    System.out.println("Name: Wesley Mutisya");
+    System.out.println("Goal: To become a Java based full stack developer");
+    System.out.println("Excited to learn: Object-Oriented Programming");
+  }
+}
