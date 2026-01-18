e878fbd4038c81b188812d38e98e19f0cad3fd03
wessgitley <wesmut00@gmail.com>
2026-01-18 16:36:51 +0300

    Simple Calculator program

---
+++ Operators/CalculatorProgram.java
@@ -0,0 +1,24 @@
+package Operators;
+
+import java.util.Scanner;
+
+public class CalculatorProgram {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter first number: ");
+    int firstValue = scanner.nextInt();
+    System.out.println("Enter second number: ");
+    int secondValue = scanner.nextInt();
+    int sum = firstValue + secondValue;
+    System.out.println("The sum is: " + sum);
+    int sub = firstValue - secondValue;
+    System.out.println("The difference is: " + sub);
+    double div = (double) (firstValue / secondValue);
+    System.out.println("The result for the division is: " + div);
+    int mul = firstValue * secondValue;
+    System.out.println("The product is: " + mul);
+    int modul = firstValue % secondValue;
+    System.out.println("The result of the modulus is: " + modul);
+    scanner.close();
+  }
+}
