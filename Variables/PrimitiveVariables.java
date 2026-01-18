7908e96563665195610584344665a5d850a87e09
wessgitley <wesmut00@gmail.com>
2026-01-18 15:57:08 +0300

    Primitive Variables

---
+++ Variables/PrimitiveVariables.java
@@ -0,0 +1,19 @@
+package Variables;
+
+public class PrimitiveVariables {
+  // public PrimitiveTypes() {
+  // variables
+  // }
+
+  public static void main(String[] args) {
+    int age = 25;
+    long bigNumber = 123456789L;
+    float price = 19.99F;
+    double precise = 3.1415926535;
+    char grade = 'A';
+    boolean isJavaFun = true;
+    int MAX_USERS = true;
+    System.out.println("Age: " + age);
+    System.out.println("Grade: " + grade);
+  }
+}
